8107d1311da9010355574f6dc018fad85673c4a2
Celso Yamashita <ctyamashita@hotmail.com>
2022-05-09 17:09:04 +0900

    done

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,31 @@
+require 'json'
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    until @letters.length == 10
+      @letters << ('a'..'z').to_a.sample
+      @letters.uniq!
+    end
+  end
+
+  def score
+    attempt = params[:attempt]
+    letters_array = params[:letters].chars.select { |char| char.match(/[a-z]/) }
+    # checking valid word
+    valid_answer = attempt.chars.all? { |letter| letters_array.include?(letter) }
+    url = "https://wagon-dictionary.herokuapp.com/#{attempt}"
+    json = URI.open(url).read
+    hash = JSON.parse(json)
+    # ##
+    @result = "Sorry but #{attempt} does not seem to be a valid English word."
+    @result = "Sorry but #{attempt} can't be built out of #{letters_array.join(', ').upcase}" if hash['found']
+    @result = "Congratulations! #{attempt} is a valid English word!" if hash['found'] && valid_answer
+    @result = "Don't use repeated letters" if attempt.chars.uniq != attempt.chars
+
+    @score = attempt.size * 10
+    @score = 0 if hash['found'] && valid_answer && attempt.chars.uniq != attempt.chars
+    # raise
+  end
+end
